fb7c386d1853a308643b3258b8fdce0ed365a56c
vurtal248 <chadwayan@gmail.com>
2026-03-18 19:36:39 -0600

    Update chrome.js

--- chrome.js
+++ chrome.js
@@ -3,7 +3,7 @@
 // @version        1.0
 // @author         vur
 // @description    JS
-// @compatibility  Zen Browser
+// @compatibility  Firefox 100+
 // ==/UserScript==
  
 (function InjectFunction() {
