b723f256fcc9f0d75e2bda1b64f3f7927b333093
vurtal248 <chadwayan@gmail.com>
2026-04-14 19:37:20 -0600

    Update Animations.uc.js

--- Animations.uc.js
+++ Animations.uc.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name           Animations
-// @version        1.4.13
+// @version        1.4.14
 // @author         vur
 // @description    JS
 // @compatibility  Firefox 100+
